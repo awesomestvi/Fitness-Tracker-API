d41d6196152a995033e646964bcd46fdf450a744
awesomestvi <vishal.cn.92@gmail.com>
2021-09-04 03:29:31 +0200

    Adding static html to root

--- index.js
+++ index.js
@@ -4,8 +4,45 @@ const database = require("./data.json");
 
 let port = process.env.PORT || 3000;
 
+const html = `
+<style>
+body,
+html {
+    margin:0 auto;
+    padding:0;
+    box-sizing:border-box;
+    background:#303030;
+    color:#fff;
+    font-family:Roboto,Helvetica Neue,sans-serif;
+    text-align:center;
+}
+body{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    width:330px;
+    height: 100vh;
+}
+h1{
+    padding:20px;
+    font-size:24px;
+}
+a {
+    color:#f48fb1;
+    text-decoration:none;
+}
+a:hover, a:focus, a:visited {
+    color:#f48fb1;
+}
+</style>
+<h1>Fitness Tracker API</h1>
+<div>List of all exercises - <a href="/api/exercises">/api/exercises</a></div>
+`;
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(html);
 });
 
 app.get("/api/exercises", (req, res) => {
